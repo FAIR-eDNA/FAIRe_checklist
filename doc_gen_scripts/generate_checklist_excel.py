16bbcbc30d59fb3824a5003b53e230705f5138dd
sformel <steve@formeldataservices.com>
2025-08-07 05:58:00 -0500

    fix incorrect text in excel file readme tab, close #7

--- doc_gen_scripts/generate_checklist_excel.py
+++ doc_gen_scripts/generate_checklist_excel.py
@@ -105,16 +105,16 @@ bold = Font(bold=True)
 
 # === Write instructions ===
 instructions = [
-    'Date and time must be recorded following ISO 8601 format (yyyy-mm-ddThh:mm:ss)...',
-    "Time duration must be recorded following ISO 8601 durations (PnYnMnWnDTnHnMnS...)",
-    "A date and time range can be specified using a forward slash (/) ...",
-    "Use space vertical bar space ( | ) to separate multiple values in a list...",
-    "Use hyphen (-) to indicate a range of numeric or integer values...",
-    "For numeric fields, enter only numbers and do not enter units...",
+    'Date and time must be recorded following ISO 8601 format (yyyy-mm-ddThh:mm:ss). Time zone must be specified after the timestamp e.g., "2008-01-23T19:23-06:00" in the time zone six hours earlier than UTC, or "2008-01-23T19:23Z" at UTC time. In Excel, format the cell as text to prevent from automatic conversion to other date fromats.',
+    "Time duration must be recorded following ISO 8601 durations (PnYnMnWnDTnHnMnS for P<date>T<time>). e.g., P1Y1M1DT1H1M1.1S = One year, one month, one day, one hour, one minute, one second, and 100 milliseconds",
+    "A date and time range can be specified using a forward slash (/) to separate the start and end values (e.g., 2008-01-23T19:23-06:00/2008-01-23T19:53-06:00).",
+    "Use space vertical bar space ( | ) to separate multiple values in a list. i.e., x | y | z",
+    "Use hyphen (-) to indicate a range of numeric or integer values. In Excel, format the cell as text to prevent it from being auto-formatted as a date.",
+    "For numeric fields, enter only numbers and do not enter units. Read the descriptions for the standard unit.",
     "For Boolean type of questions, always read the descriptions and answer with 0 or 1.",
-    'When "other" is selected..., write the answer using the format of "other: FREE TEXT DESCRIPTION".',
-    "If suitable term names are not available..., users should search for them in MIxS or DwC...",
-    "When a value is missing from a mandatory term..., use the INSDC missing value vocabulary..."
+    'When "other" is selected in a controlled vocabulary term, write the answer using the format of "other: FREE TEXT DESCRIPTION".',
+    "If suitable term names are not available in the current checklist, users should search for them in existing standards, such as MIxS (https://genomicsstandardsconsortium.github.io/mixs/) and DwC (https://dwc.tdwg.org/terms/), and use these standardized terms where possible. If relevant terms cannot be found in these resources, users may add new terms using clear, concise, and descriptive names within related tables.",
+    "When a value is missing from a mandatory term, it is required to provide the reason using the INSDC missing value controlled vocabulary (https://www.insdc.org/submitting-standards/missing-value-reporting/). See https://fair-edna.github.io/guidelines.html#missing-values for the full list of values."
 ]
 
 ws_readme.cell(row=1, column=1, value="Instructions for data submitters:").font = bold
